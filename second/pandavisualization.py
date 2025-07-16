6c77e969d9b42004cd5f4b86eb6af06b92e35844
Irem Aktay Alkaya <aktayirem@gmail.com>
2025-07-16 15:06:50 +0300

    Add pandas visualization script

---
+++ second/pandavisualization.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df = pd.read_csv("sequence_stats.csv")
+print(df)
+
+import matplotlib.pyplot as plt
+
+plt.hist(df["GC_Content"], bins=10, color='skyblue', edgecolor='black')
+plt.title("GC İçeriği Dağılımı")
+plt.xlabel("GC İçeriği (%)")
+plt.ylabel("Sekans Sayısı")
+plt.grid(True)
+plt.tight_layout()
+plt.show()
